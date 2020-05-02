8b0f95fe238e5b12e5fcc8de344e3029de39d477
Damir <bakhtiyev.damir@gmail.com>
2020-05-02 19:44:21 +0600

    Project init

---
+++ src/test/java/com/saladinjava/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saladinjava.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
