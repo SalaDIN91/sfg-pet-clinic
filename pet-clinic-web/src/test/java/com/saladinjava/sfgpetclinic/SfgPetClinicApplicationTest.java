2153c7f2c6dd2614de052dc8d86b27e88096d68b
Damir <bakhtiyev.damir@gmail.com>
2020-05-05 20:52:40 +0600

    Multi Module packaging success

--- pet-clinic-web/src/test/java/com/saladinjava/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/com/saladinjava/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -1,10 +1,10 @@
 package com.saladinjava.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+class SfgPetClinicApplicationTest {
 
     @Test
     void contextLoads() {
