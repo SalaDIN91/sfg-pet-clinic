8bd2d780b32e9cd7d5cdb967943e487d5dc0e128
Damir <bakhtiyev.damir@gmail.com>
2020-05-06 21:52:39 +0600

    Service refactor. closes #1

--- pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,9 @@ package com.saladinjava.sfgpetclinic.services;
 
 import com.saladinjava.sfgpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/services/VetService.java
@@ -2,15 +2,9 @@ package com.saladinjava.sfgpetclinic.services;
 
 import com.saladinjava.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
     Vet findByLastName(String lastName);
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
