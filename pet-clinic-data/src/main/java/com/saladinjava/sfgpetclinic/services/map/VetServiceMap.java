7322fe3e190a1a9a99be978a96adbd5453a1cfcb
Damir <bakhtiyev.damir@gmail.com>
2020-05-06 23:30:14 +0600

    Impl Map based services

---
+++ pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.saladinjava.sfgpetclinic.services.map;
+
+import com.saladinjava.sfgpetclinic.model.Vet;
+import com.saladinjava.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService <Vet, Long> implements CrudService <Vet, Long>{
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
