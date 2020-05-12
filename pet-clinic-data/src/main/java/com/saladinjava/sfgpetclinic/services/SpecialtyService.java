f7c21e5eb3407339a7e2fe065dbd34cabcb09753
Damir <bakhtiyev.damir@gmail.com>
2020-05-12 20:55:40 +0600

    Added Specialties map impl

---
+++ pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package com.saladinjava.sfgpetclinic.services;
+
+
+import com.saladinjava.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService <Specialty, Long>{
+}
