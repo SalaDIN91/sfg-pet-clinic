3c6ccb9099395305c9dbbfb6d0b1ce5361ee68a0
Damir <bakhtiyev.damir@gmail.com>
2020-05-12 09:50:49 +0600

    updated Owner with contact info

--- pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/model/Owner.java
@@ -6,15 +6,17 @@ import java.util.Set;
 public class Owner extends Person {
 
     private Set<Pet> pets;
-    private Owner owner;
     private LocalDate birthDate;
+    private String address;
+    private String city;
+    private String telephone;
 
-    public Owner getOwner() {
-        return owner;
+    public Set<Pet> getPets() {
+        return pets;
     }
 
-    public void setOwner(Owner owner) {
-        this.owner = owner;
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 
     public LocalDate getBirthDate() {
@@ -25,11 +27,27 @@ public class Owner extends Person {
         this.birthDate = birthDate;
     }
 
-    public Set<Pet> getPets() {
-        return pets;
+    public String getAddress() {
+        return address;
     }
 
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
