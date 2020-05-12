d7375aaaebfe4ba77dcdf1908d935f796e1a1823
Damir <bakhtiyev.damir@gmail.com>
2020-05-12 21:45:34 +0600

    fixed Links

--- pet-clinic-web/src/main/java/com/saladinjava/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/saladinjava/sfgpetclinic/controllers/VetController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping({"/vets","/vets.html"})
 @Controller
 public class VetController {
 
@@ -20,4 +20,5 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
 }
