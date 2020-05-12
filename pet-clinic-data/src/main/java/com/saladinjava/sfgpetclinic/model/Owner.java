f1ce16a415c7329c4e01ea9503b87cdb64651655
Damir <bakhtiyev.damir@gmail.com>
2020-05-12 09:40:42 +0600

    added visits, updated owner with pet relations

--- pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/saladinjava/sfgpetclinic/model/Owner.java
@@ -1,4 +1,35 @@
 package com.saladinjava.sfgpetclinic.model;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
